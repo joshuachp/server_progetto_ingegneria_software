246a066c072bb19fe6cd566a972e0d471e1788af
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-06-03 18:36:00 +0200

    Added tables for some base data structures

---
+++ src/main/java/org/example/server/models/Section.java
@@ -0,0 +1,11 @@
+package org.example.server.models;
+
+public class Section {
+    private final Integer id;
+    private final String name;
+
+    public Section(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
